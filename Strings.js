baeafd1b80cac6598ee41e4a0ec74b71f17e0fd6
Anthony Gonzales <anthony.j.gonzales22gmail.com>
2021-11-22 14:54:24 -0600

    Types,operators and strings

---
+++ Strings.js
@@ -0,0 +1,4 @@
+var myName = "My name is Anthony.\n";
+var fact1 = "In my free time I like to ride motorcycles.\n"
+var fact2 = "On the weekends I enjoy spending time outdoors.\n"
+console.log (myName + fact1 + fact2)
