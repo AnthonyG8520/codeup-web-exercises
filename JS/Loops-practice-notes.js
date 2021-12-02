8b14b01211884b7e59142ff443b8cb14e6670638
Anthony Gonzales <anthony.j.gonzales22@gmail.com>
2021-12-02 16:57:16 -0600

    loop practice and exercises

---
+++ JS/Loops-practice-notes.js
@@ -0,0 +1,106 @@
+//Loops
+
+//--While--
+
+//syntax
+// while (condition){
+//     // execute this code
+//
+// }
+
+
+// let parkingSpotsAvail = 20;
+// let isFull = console.log("The parking lot is full.")
+
+//syntax
+// let i = 0
+// while(i < 10){
+//     console.log(i);
+//     i++; // i++ = i + 1
+// }
+
+// while(parkingSpotsAvail != 0){
+//     console.log(parkingSpotsAvail + " parking spots available.")
+//     parkingSpotsAvail--
+//     console.log(parkingSpotsAvail + " parking spots available.")
+// }
+
+// while(parkingSpotsAvail >= 0) {
+//     console.log(parkingSpotsAvail + " parking spots available.")
+//     parkingSpotsAvail--
+// }
+
+//--do-while--
+//ALWAYS RUNS FIRST and then checks the condition
+// let i = 20
+// do{
+//     console.log("i before i++:" + i)
+//     i++;
+//     console.log("i after i++:" + i)
+// }while(i <= 15)
+
+// let i = 0
+//
+// do{
+//     console.log(i);
+//     i++;
+//     i++;
+// }while(i <= 50)
+
+// practice
+// let i = 0
+// while(i <= 10){
+//     console.log(i)
+//     i++
+// }
+// console.log("-----------")
+//
+// let b = 0
+// do{
+//     b++
+//     console.log(b)
+// }while(b <= 10)
+
+// let passWord = "1234";
+// let userPassWord = prompt("Enter password.");
+//
+// !!!!!DOESNT WORK!!!!!
+// do{
+//     userPassWord
+//     if(passWord != userPassWord){
+//         alert("Wrong password");
+//     }
+// }while (passWord != userPassWord);
+
+
+//--For Loop--
+
+//syntax
+// for (1; 2; 3){
+//     //code
+// };
+//
+// 1. Starting point;
+// 2. Stopping point / condition;
+// 3. How to iterate;
+
+
+// for(let i = 0; i<=15; i++){
+//     console.log("Num is:" + i);
+// }
+
+// To iterate by given number
+// for(let i = 0; i<=15; i+=5){
+//     console.log("Num is:" + i);
+// }
+
+// NESTED LOOPS
+
+// for(let counter = 1; counter <=5; counter++){
+//     console.log('FIRST counter:' + counter);
+//     // count from 1 to 5
+//     for(let counterTwo = 5; counterTwo >0; counterTwo--){
+//         console.log("SECOND counter" + counterTwo);
+//     }// counts from 5 to 1
+//
+// }
---
+++ JS/for loops.js
@@ -0,0 +1,32 @@
+//For loops
+function showMultiplicationTable(input){
+    for(let i = 1; i <= 10; i++) {
+        console.log(input * i)
+    }
+}
+showMultiplicationTable(5)
+
+
+for(let i = 1; i <= 10; i++){
+    var random = Math.floor((Math.random() * 181) + 20);
+    if(random % 2 === 0){
+        console.log(random + " is even")
+    }else console.log(random + " is odd")
+}
+
+let totalNumberofRows = 9;
+let output = '';
+for (let i = 1; i <= totalNumberofRows; i++) {
+    for (let j = 1; j <= i; j++) {
+        output += i;
+    }
+    console.log(output);
+    output = '';
+}
+
+for(let i = 100; i >= 5; i-=5){
+    console.log(i)
+}
+//Break and continue
+
+
