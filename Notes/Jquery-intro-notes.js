265a838fe2ea8a86143814202927a11d57309502
Anthony Gonzales <anthony.j.gonzales22@gmail.com>
2022-01-20 10:20:42 -0600

    added more jquery notes

--- Notes/Jquery-intro-notes.js
+++ Notes/Jquery-intro-notes.js
@@ -32,9 +32,46 @@ let user1 = {
 
 let loginStatus = user1.isLoggedIn;
 
+//selecting by id and adding class
 if(loginStatus === true){
     $("#heading").addClass("border")
 }
+
+//selecting element by class amd adding css
+$(".blue-bg").css("background-color", "blue")
+
+
+// selecting by tag name and changing font size
+$("li").css("font-size", "24px")
+
+//changing multiple css properties
+$('#p-one').css('font-size', '8px').css('color', 'magenta')
+$('#p-one').css({'color': 'red', 'font-size': '8px'})
+
+
+//styling multiple elements
+$('p, ul li').css
+
+// style *ALL* elements
+$('*').css
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
