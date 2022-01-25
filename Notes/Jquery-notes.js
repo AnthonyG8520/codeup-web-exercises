fd20e42dbf7cc46f0fee256473dd17e8b4b7036f
Anthony Gonzales <anthony.j.gonzales22@gmail.com>
2022-01-25 15:21:43 -0600

    added jquery notes

--- Notes/Jquery-notes.js
+++ Notes/Jquery-notes.js
@@ -136,6 +136,100 @@ $('li').last().parent()
 const array1 = [1, 2, 3, 4];
 const reducer = (previousValue, currentValue) => previousValue + currentValue
 
+//---------------Ajax Requests--jquery---------------------
+
+//AJAX - Asynchronous JavaScript and XML (and JSON) -->
+//asynchronous - not at the same time - one thing running while something else is going on
+//synchronous - at the same time
+//xml - eXtensible Markup Language -- designed to store and transport data
+//js
+//Web Server - server that holds your website
+// // software: handle requests, sends data, understands URLs, allows us to interact with servers, hosts files,
+// // hardware: computers storing data
+//HTTP Server - software that understands URLs and is able to communicate with the browser to view webpages
+//JSON - JavaScript Object Notation
+//JSON object syntax
+// {'team': 'spurs',
+//     'wins': 7
+// }
+
+// API - application programming interface -
+
+
+//AJAX Request(jquery syntax)
+//get info from ur random dog API
+
+//$.ajax() jquery syntax for AJAX GET request
+
+$.ajax("https://dog.ceo/api/breeds/image/random") //GET
+$.ajax("url", {
+    type: "POST",
+    // data we want to save to the server:
+    data:
+})
+
+//REQUESTS:
+// GET
+// POST --> sending information (through JSON) to be stored on the server
+//DELETE --> send information about the thing you want to delete // not going to return anything (if successful)
+// PUT --> edit / send the info about the thing your editing, and typically receive the edited thing back
+
+
+// DOCUMENTATION
+
+//default type is GET
+$.ajax("url")
+
+$.ajax("url", {
+    type: "POST", //type of request
+    data: { // if POST, PUT, DELETE: SEND INFO ALONG WITH REQUEST
+        username: 'anthony',
+        post: 'doing javascript'
+    }
+});
+
+
+//GET - RESPONSE
+
+// .done() tells when a request is done being made / accepts a function on what to do if request finishes
+$.ajax("url").done(function(data, status){
+    console.log("request is done")
+});
+
+//.fail tells when a request fails / accepts a function on what to do if request fails
+$.ajax("url").fail(function(){
+    console.log("your request has failed")
+});
+
+//request an image of a dog from DOG CEO API and render it dynamically on the HTML page
+
+//select the element we want to change
+//change its src attribute and give it a value
+//retrieve data from API
+// handle the response
+//retrieve the actual item we need from the resonse // data.message
+// --> string
+//passs the string recieved into the value of src being changed
+
+
+//sends the request
+$.ajax("https://dog.ceo/api/breeds/image/random").done(function(data, status){
+    //.done handling the response here
+    // data is the object that is returned and .message return the message value from within the object
+    console.log(data.message);
+        //status returns the status of the request
+    console.log(status);
+    // inputs the url directly into the element were trying to modify
+    $("#doggo").attr("src", data.message)
+});
+
+$.ajax("url").always(function() {
+    // .always accepts a function that tells what to do no matter the outcome of the request
+})
+
+
+
+
 
 
 
