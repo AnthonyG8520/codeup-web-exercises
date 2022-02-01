f2806d2d32b78dc340695688933027aa5057f475
Anthony Gonzales <anthony.j.gonzales22@gmail.com>
2022-02-01 14:11:05 -0600

    work on weather map

--- JS/openweathermap.js
+++ JS/openweathermap.js
@@ -1,25 +1,93 @@
-function timeConverter(UNIX_timestamp){
+function timeConverter(UNIX_timestamp) {
     var a = new Date(UNIX_timestamp * 1000);
-    var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
     var year = a.getFullYear();
     var month = months[a.getMonth()];
     var date = a.getDate();
-    var time = month + ' ' + date + ' ' + year ;
+    var time = month + ' ' + date + ' ' + year;
     return time;
 }
 
+mapboxgl.accessToken = mapbox_maps_key;
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v9',
+    zoom: 10,
+    center: [-98.4916, 29.4252]
+});
+
+function getWeather(lat, lng) {
+    $("#weather").empty()
+    $.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lng}&units=imperial&exclude={part}&appid=${weathermap_key}`).done(function (data) {
+        console.log(data)
+        for (var i = 0; i <= 4; i++) {
+            let date = timeConverter(data.daily[i].dt);
+            let weatherEntry = `<div class="col card mx-3" id="day-weather">
+                                        <div class="card-header">${date}</div>
+                                        <img id="weather-icon" src="https://openweathermap.org/img/w/${data.daily[i].weather[0].icon}.png" alt="">
+                                        <div class="card-title">${data.daily[i].weather[0].description}</div>
+                                        <div class="card-text">Low: ${data.daily[i].temp.min} / High: ${data.daily[i].temp.max}</div>
+                                        <div class="card-text">Humidity: ${data.daily[i].humidity}%</div>
+                                    </div>`
+            $("#weather").append(weatherEntry);
+        }
+    });
+}
 
 $.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${29.4241}&lon=${-98.4936}&units=imperial&exclude={part}&appid=${weathermap_key}`).done(function(data){
     console.log(data)
     for (var i = 0; i <= 4; i++) {
         let date = timeConverter(data.daily[i].dt);
-        let weatherEntry = `<div id="day-weather">
-                                <div>${date}</div>
-                                <img src="https://openweathermap.org/img/w/${data.daily[i].weather[0].icon}.png" alt="">
-                                <div>Low: ${data.daily[i].temp.min} / High: ${data.daily[i].temp.max}</div>
-                                <div>Humidity: ${data.daily[i].humidity}%</div>
-                                <div>${data.daily[i].weather[0].description}</div>
+        let weatherEntry = `<div class="col card" id="day-weather">
+                                <div class="card-header">${date}</div>
+                                <img id="weather-icon" src="https://openweathermap.org/img/w/${data.daily[i].weather[0].icon}.png" alt="">
+                                <div class="card-title">${data.daily[i].weather[0].description}</div>
+                                <div class="card-text">Low: ${data.daily[i].temp.min} / High: ${data.daily[i].temp.max}</div>
+                                <div class="card-text">Humidity: ${data.daily[i].humidity}%</div>
                             </div>`
         $("#weather").append(weatherEntry);
     }
+});
+
+const marker = new mapboxgl.Marker({
+    draggable: true
+})
+    .setLngLat([-98.4916, 29.4252])
+    .addTo(map);
+
+function onDragEnd() {
+    const lngLat = marker.getLngLat();
+    console.log(lngLat)
+    getWeather(lngLat.lat, lngLat.lng)
+};
+
+marker.on('dragend', onDragEnd);
+
+$("#submit").click(function () {
+    console.log("clicked")
+    var searchValue = $("#search-city").val()
+    console.log(searchValue)
+    $("#weather").empty()
+    geocode(searchValue, mapboxgl.accessToken).then(function (result) {
+        console.log(result);
+        map.setCenter(result);
+        map.setZoom(15);
+        var marker = new mapboxgl.Marker()
+            .setLngLat(result)
+            .addTo(map)
+        $.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${result[1]}&lon=${result[0]}&units=imperial&exclude={part}&appid=${weathermap_key}`).done(function (data) {
+            console.log(data)
+            for (var i = 0; i <= 4; i++) {
+                let date = timeConverter(data.daily[i].dt);
+                let weatherEntry = `<div class="col card mx-3" id="day-weather">
+                                        <div class="card-header">${date}</div>
+                                        <img id="weather-icon" src="https://openweathermap.org/img/w/${data.daily[i].weather[0].icon}.png" alt="">
+                                        <div class="card-title">${data.daily[i].weather[0].description}</div>
+                                        <div class="card-text">Low: ${data.daily[i].temp.min} / High: ${data.daily[i].temp.max}</div>
+                                        <div class="card-text">Humidity: ${data.daily[i].humidity}%</div>
+                                    </div>`
+                $("#weather").append(weatherEntry);
+            }
+        });
+    });
 });
