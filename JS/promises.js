c03e960f9ef1d7d17024952fc22b1888b7b1601e
Anthony Gonzales <anthony.j.gonzales22@gmail.com>
2022-02-08 15:59:59 -0600

    promises exercise done

---
+++ JS/promises.js
@@ -0,0 +1,25 @@
+function getLastCommit(owner, repo) {
+    fetch(`https://api.github.com/repos/${owner}/${repo}/commits`, {headers: {"Authorization": github_api_key}})
+        .then(res => res.json())
+        .then(result => console.log(result[0].commit.author.date))
+}
+
+getLastCommit('AnthonyG8520', 'codeup-web-exercises')
+
+
+// fetch(`https://api.github.com/users/AnthonyG8520`, {headers: {"Authorization": github_api_key}})
+//     .then(res => res.json())
+//     .then(result => console.log(result))
+
+
+function wait (time) {
+   return new Promise((resolve, reject) => {
+        setTimeout(() => {
+                resolve('Successful');
+                reject('Failed');
+        }, time);
+    });
+}
+
+wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
