68b806ac4dc2e8b2da058e8d25f9a93c47948f52
AnthonyG8520 <anthony.j.gonzales22@gmail.com>
2022-08-12 04:05:16 -0500

    brushing on primitive type manipulation

--- JS/scratch.js
+++ JS/scratch.js
@@ -1,3 +1,14 @@
-for (let i = 0; i > 5; i++){
-    console.log(i)
+function disemvowel(str){
+    let checkStr = str.toLowerCase()
+    let newStr = ""
+    for(let i = 0; i <= str.length - 1; i++){
+        if(checkStr[i] == "a" || checkStr[i] == "e" || checkStr[i] == "i" || checkStr[i] == "o" || checkStr[i] == "u"){
+            continue
+        }
+        newStr += str[i]
+    }
+    return newStr
 }
+
+console.log(disemvowel("Hello"));
+
