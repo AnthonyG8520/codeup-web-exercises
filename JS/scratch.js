6d33ad16e9f57f99ed0dcdd2436c250f17b023e3
AnthonyG8520 <anthony.j.gonzales22@gmail.com>
2022-08-12 04:29:24 -0500

    brushing up on primitive type manipulation

--- JS/scratch.js
+++ JS/scratch.js
@@ -1,14 +1,32 @@
 function disemvowel(str){
-    let checkStr = str.toLowerCase()
-    let newStr = ""
+    let checkStr = str.toLowerCase();
+    let newStr = "";
     for(let i = 0; i <= str.length - 1; i++){
-        if(checkStr[i] == "a" || checkStr[i] == "e" || checkStr[i] == "i" || checkStr[i] == "o" || checkStr[i] == "u"){
+        if(checkStr.charAt(i) === "a" || checkStr.charAt(i) === "e" || checkStr.charAt(i) === "i" || checkStr.charAt(i) === "o" || checkStr.charAt(i) === "u"){
             continue
         }
-        newStr += str[i]
+        newStr += str.charAt(i)
     }
-    return newStr
+    return newStr;
+}
+
+
+function XO(str){
+    let checkStr = str.toLowerCase();
+    let oCount = 0;
+    let xCount = 0;
+    for(let i = 0; i < str.length; i++){
+        if(checkStr.charAt(i) === "x"){
+            xCount++
+        }
+        else if(checkStr.charAt(i) === "o"){
+            oCount++
+        }
+    }
+    return oCount === xCount
 }
 
 console.log(disemvowel("Hello"));
+
+console.log(XO("XOooxx"))
 
